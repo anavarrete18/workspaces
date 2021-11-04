7dd173863f0a744364e1b2591857af05590f2610
Angel Navarrete Sanchez <anavarrete18@ilg.cat>
2021-11-04 19:00:14 +0000

    Angel Navarrete: vs code nivell1ex

--- java/M5_test2/src/App.java
+++ java/M5_test2/src/App.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
-        System.out.print("\nEscriu el teu nom: ");
+        System.out.print("\n Escriu el teu nom: ");
         String nom = in.nextLine();
-        System.out.println("\nHola " + nom + "!!\n");
+        System.out.println("\n Hola " + nom + "!!\n");
         in.close();
     }
 }
